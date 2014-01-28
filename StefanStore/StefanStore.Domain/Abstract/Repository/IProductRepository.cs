5393f784ab209e25fdff4a1275b92cfd24655d2d
stefansperanta <stefan.speranta@yahoo.com>
2014-01-28 10:56:48 +0200

    Revert "add stefanstore solution"
    This reverts commit a81ed82b82751b202058c6b23f8a6d98791f38f0.

--- StefanStore/StefanStore.Domain/Abstract/Repository/IProductRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Linq;
-
-namespace StefanStore.Domain.Abstract
-{
-    public interface IProductRepository : IGenericRepository<Product>
-    {
-        IQueryable<Product> GetProductsByCategory(string categoryName); 
-    }
-}
